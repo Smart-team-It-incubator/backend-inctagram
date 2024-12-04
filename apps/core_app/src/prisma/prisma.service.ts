cd742dbd988d5c2716997ec8d99e8bde6cfd91f9
Evgenii Konstantinov <102171281+Trychangelife@users.noreply.github.com>
2024-12-05 00:03:57 +0300

    deploy #2

--- apps/core_app/src/prisma/prisma.service.ts
+++ apps/core_app/src/prisma/prisma.service.ts
@@ -5,16 +5,10 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
     // Выбираем базу данных в зависимости от окружения
-
-    // console.log('NODE_ENV:', process.env.NODE_ENV);
-    // console.log('DATABASE_URL_PROD:', process.env.DATABASE_URL_PROD);
-    // console.log('DATABASE_URL_DEV:', process.env.DATABASE_URL_DEV);
     const databaseUrl =
       process.env.NODE_ENV === 'production'
         ? process.env.DATABASE_URL_PROD
         : process.env.DATABASE_URL_DEV;
-
-    // console.log(process.env.NODE_ENV === 'production', process.env.NODE_ENV);
 
     // Передаём URL в PrismaClient
     super({
