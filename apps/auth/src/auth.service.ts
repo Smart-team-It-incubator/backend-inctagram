a7838caf6c12c8549b918864886046cf58157242
Evgenii Konstantinov <102171281+Trychangelife@users.noreply.github.com>
2025-01-06 09:20:09 +0300

    Update auth.service.ts

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -193,7 +193,7 @@ export class AuthService {
 
   async _generateHash(password: string): Promise<string> {
     try {
-      const hash = await bcrypt.hash(password, 10);
+      const hash = await bcrypt.hash(password, 5);
       return hash
     } catch (error) {
       console.error("Error in generateHash:", error.message);
