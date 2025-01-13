b7637c66848d951bf1b312bd31c1d4e7e75d9ddc
Evgenii Konstantinov <102171281+Trychangelife@users.noreply.github.com>
2025-01-13 08:15:44 +0300

    Update main.ts

--- apps/core_app/src/main.ts
+++ apps/core_app/src/main.ts
@@ -17,8 +17,8 @@ async function bootstrap() {
       .build();
     const coreDoc = SwaggerModule.createDocument(app, config);
     // Получение документации для auth микросервиса
-    //const authDoc = await axios.get(`https://auth.smart-reg.org.ru/api/v1-json`); // Путь к Swagger документации для auth, в настоящем пути "-json" нет, но это необходимо указать для того чтобы склеить документацию.
-    const authDoc = await axios.get(`http://localhost:4000/api/v1-json`); // Путь к Swagger документации для auth, в настоящем пути "-json" нет, но это необходимо указать для того чтобы склеить документацию.
+    const authDoc = await axios.get(`https://auth.smart-reg.org.ru/api/v1-json`); // Путь к Swagger документации для auth, в настоящем пути "-json" нет, но это необходимо указать для того чтобы склеить документацию.
+    //const authDoc = await axios.get(`http://localhost:4000/api/v1-json`); // Путь к Swagger документации для auth, в настоящем пути "-json" нет, но это необходимо указать для того чтобы склеить документацию.
     //console.log(authDoc.data)
     const combinedDoc = {
       ...coreDoc,
