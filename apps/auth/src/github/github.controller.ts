8ef387009defb06cb2e91ab7c1b22c3af816cc05
Evgenii Konstantinov <102171281+Trychangelife@users.noreply.github.com>
2025-01-22 15:33:01 +0300

    Update github.controller.ts

--- apps/auth/src/github/github.controller.ts
+++ apps/auth/src/github/github.controller.ts
@@ -69,8 +69,8 @@ export class GithubAuthController {
       });
 
       // Редирект на нужную страницу
-      return res.redirect('http://localhost:3000/'); // Редирект на страницу после отправки токенов
-      //return res.redirect('http://smart-reg.org.ru/'); // Редирект на страницу после отправки токенов
+      //return res.redirect('http://localhost:3000/'); // Редирект на страницу после отправки токенов
+      return res.redirect('https://smart-reg.org.ru/'); // Редирект на страницу после отправки токенов
     }
 
     // 4. Если пользователь есть, но GitHub не привязан, привязываем
@@ -93,8 +93,8 @@ export class GithubAuthController {
       });
 
       // Редирект на нужную страницу
-      return res.redirect('http://localhost:3000/'); // Редирект на страницу после отправки токенов
-      //return res.redirect('http://smart-reg.org.ru/'); // Редирект на страницу после отправки токенов
+      //return res.redirect('http://localhost:3000/'); // Редирект на страницу после отправки токенов
+      return res.redirect('https://smart-reg.org.ru/'); // Редирект на страницу после отправки токенов
     }
 
     // 5. Если пользователь не найден, регистрируем нового
@@ -133,8 +133,8 @@ export class GithubAuthController {
       });
 
       // Редирект на нужную страницу после регистрации
-      return res.redirect('http://localhost:3000/'); // Редирект на страницу после отправки токенов
-      //return res.redirect('http://smart-reg.org.ru/'); // Редирект на страницу после отправки токенов
+      //return res.redirect('http://localhost:3000/'); // Редирект на страницу после отправки токенов
+      return res.redirect('https://smart-reg.org.ru/'); // Редирект на страницу после отправки токенов
     }
 
     // На случай, если все варианты не сработают
