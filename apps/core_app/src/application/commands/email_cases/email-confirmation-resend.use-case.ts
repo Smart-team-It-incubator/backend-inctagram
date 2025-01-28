7191296040920d108033c0bc61f5f0a9b715ccd2
Evgenii Konstantinov <102171281+Trychangelife@users.noreply.github.com>
2025-01-28 17:38:09 +0300

    Update email-confirmation-resend.use-case.ts

--- apps/core_app/src/application/commands/email_cases/email-confirmation-resend.use-case.ts
+++ apps/core_app/src/application/commands/email_cases/email-confirmation-resend.use-case.ts
@@ -37,12 +37,8 @@ export class ResendConfirmationCodeUseCase {
         }
 
         //Запуск отправки Email в фоне, т.к возможно из-за VPN проблемы связи с email-server, для повторной отправки сделаем Email-Resending
-        this.emailService.sendEmailConfirmationMessage(command.email, resultUserUpdate.emailConfirmationCode).catch((error) => {
-            console.error('Failed to send email:', error);
-        });
-        // await this.emailProducerService.sendEmailConfirmationMessage({email: command.email, confirmationCode: resultUserUpdate.emailConfirmationCode}).catch((error) => {
-        //     console.error('Failed to send email:', error);
-        // })
+        this.emailService.sendEmailConfirmationMessage(command.email, resultUserUpdate.emailConfirmationCode)
+        // await this.emailProducerService.sendEmailConfirmationMessage({email: command.email, confirmationCode: resultUserUpdate.emailConfirmationCode})
 
         return true
     }
