a05fe391a0b1ced7e9c91885f37434cb5fa7d53e
Evgenii Konstantinov <102171281+Trychangelife@users.noreply.github.com>
2025-02-01 20:23:26 +0300

    Update http-exception.filter.ts

--- libs/filters/src/http-exception.filter.ts
+++ libs/filters/src/http-exception.filter.ts
@@ -1,3 +1,4 @@
+import { RabbitClientLoggerService } from '@app/rabbit_client_logger';
 import {
   ArgumentsHost,
   Catch,
@@ -8,6 +9,9 @@ import {
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
+  constructor(
+    private readonly logService: RabbitClientLoggerService
+  ) { }
   catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
@@ -20,6 +24,22 @@ export class HttpExceptionFilter implements ExceptionFilter {
 
     const responseBody =
       exception instanceof HttpException ? exception.getResponse() : null;
+
+    // Логируем только ошибки 500 (неожиданные ошибки)
+    if (status === HttpStatus.INTERNAL_SERVER_ERROR) {
+      const errorMessage =
+        exception instanceof Error ? exception.message : 'Unknown error';
+
+      // Логируем ошибку в RabbitMQ через RabbitClientLoggerService
+      this.logService.error({
+        message: errorMessage,
+        additionalInfo: {
+          requestId: request.headers['x-request-id'], // можешь добавить свой ID запроса
+          userId: request.user?.id, // если есть информация о пользователе
+          errorStack: exception.stack,
+        },
+      });
+    }
 
     // Проверяем, является ли это ошибка от class-validator, если да, то возвращаем без изменений
     if (
