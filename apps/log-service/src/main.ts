64f2e2885b69625794a6d24866146b9b59e8f0ae
Evgenii Konstantinov <102171281+Trychangelife@users.noreply.github.com>
2025-02-01 22:28:38 +0300

    update

--- apps/log-service/src/main.ts
+++ apps/log-service/src/main.ts
@@ -9,7 +9,7 @@ import { LogModule } from './log-service.module';
 async function bootstrap() {
   // После того как конфигурация загружена, подключаем New Relic
   const { config } = await import('../../../newrelic.js'); // Загрузите конфигурацию, если нужно
-
+  console.log("Rabbit MQ URL",process.env.RABBITMQ_URL)
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(LogModule, {
     transport: Transport.RMQ,
     options: {
