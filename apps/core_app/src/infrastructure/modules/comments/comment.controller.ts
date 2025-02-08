373a6254dbadeb1c190cff22aa657c93a045ad86
Evgenii Konstantinov <102171281+Trychangelife@users.noreply.github.com>
2025-02-08 13:55:30 +0300

    swagger auth for comments

--- apps/core_app/src/infrastructure/modules/comments/comment.controller.ts
+++ apps/core_app/src/infrastructure/modules/comments/comment.controller.ts
@@ -5,7 +5,7 @@ import { CreateCommentCommand } from "@core_app/src/application/commands/comment
 import { GetCommentsByPostIdCommand } from "@core_app/src/application/queries/comments_query/get_comments.use-case";
 import { Body, Controller, Delete, Get, HttpCode, HttpException, HttpStatus, Param, Post, UseGuards } from "@nestjs/common";
 import { CommandBus } from "@nestjs/cqrs";
-import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { GetUser } from "../../utils/get-user.decorator";
 import { User } from "@core_app/src/domain/entities/user-entities";
 import { DeleteCommentCommand } from "@core_app/src/application/commands/comments_cases/delete_comment.use-case";
@@ -20,6 +20,7 @@ export class CommentController {
     @ApiOperation({ summary: 'Create a comment for a post' })
     @ApiResponse({ status: 201, description: 'Comment created successfully', type: CommentPublicDto })
     @ApiResponse({ status: 400, description: 'Failed to create comment' })
+    @ApiBearerAuth('access-token')
     @Post('create/:postId')
     async commentCreate(
         @Body() commentCreatePayload: CommentCreatePayload,
@@ -50,6 +51,7 @@ export class CommentController {
     @UseGuards(JwtAuthGuard)
     @ApiResponse({ status: 204, description: 'Comment successfully deleted' })
     @ApiResponse({ status: 404, description: 'Comment not found' })
+    @ApiBearerAuth('access-token')
     @HttpCode(204)
     @Delete('delete/:commentId')
     async deleteCommentById(@Param('commentId') commentId: string, @GetUser() user: User): Promise<boolean> {
